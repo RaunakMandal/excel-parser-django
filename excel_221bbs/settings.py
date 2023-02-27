de95e6ab0bb177465d1713169bdbe9d22eff5515
Raunak Mandal <iamriju2000@gmail.com>
2023-02-27 21:28:49 +0530

    Allow host

--- excel_221bbs/settings.py
+++ excel_221bbs/settings.py
@@ -132,3 +132,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['excel-parser-django.onrender.com']
